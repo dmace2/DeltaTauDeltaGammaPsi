2d9f2b4b72d60700d4edf6c819f4bafdba40ef1a
Dylan Mace <dtmace2@gmail.com>
2021-08-14 16:05:15 -0400

    Initial Commit

---
+++ Delt Info/Delt_InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delt_InfoApp.swift
+//  Delt Info
+//
+//  Created by Dylan Mace on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Delt_InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
